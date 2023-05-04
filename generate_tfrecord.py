a27a90749aa6554a298d63af80fd5638be0bfc9d
Zuhairaja MT <80949224+zuhairajamt@users.noreply.github.com>
2023-05-04 12:40:29 +0700

    Update generate_tfrecord.py

--- generate_tfrecord.py
+++ generate_tfrecord.py
@@ -82,14 +82,14 @@ def xml_to_csv(path):
         root = tree.getroot()
         for member in root.findall('object'):
             value = (root.find('filename').text,
-                     int(root.find('size')[0].text),
-                     int(root.find('size')[1].text),
-                     member[0].text,
-                     int(member[4][0].text),
-                     int(member[4][1].text),
-                     int(member[4][2].text),
-                     int(member[4][3].text)
-                     )
+                         int(root.find('size').find('width').text),
+                         int(root.find('size').find('height').text),
+                         member[0].text,
+                         int(member.find("bndbox").find('xmin').text),
+                         int(member.find("bndbox").find('ymin').text),
+                         int(member.find("bndbox").find('xmax').text),
+                         int(member.find("bndbox").find('ymax').text)
+                        )
             xml_list.append(value)
     column_name = ['filename', 'width', 'height',
                    'class', 'xmin', 'ymin', 'xmax', 'ymax']
